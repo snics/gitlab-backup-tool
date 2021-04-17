a359e8b7b3a2995f0fa39cf1b9aa692c02b86121
bhilgers <benhilgers@hotmail.de>
2021-04-17 18:08:15 +0200

    Add get issues

---
+++ src/issues/index.js
@@ -0,0 +1,7 @@
+const runGetIssues = require('./runGetIssues');
+const runPushIssues = require('./runPushIssues');
+
+module.exports = {
+  runGetIssues,
+  runPushIssues
+};
---
+++ src/issues/runGetIssues.js
@@ -0,0 +1,63 @@
+const argv = require('yargs')
+const fs = require('fs-extra');
+const rp = require('request-promise');
+const _ = require('lodash');
+const Promise = require('bluebird');
+const cmd = require('node-cmd');
+const cmdAsync = Promise.promisify(cmd.get, {
+  multiArgs: true,
+  context: cmd
+});
+const cliProgress = require('cli-progress');
+
+async function runGetIssues(argv) {
+  const baseUrl = argv.url || 'https://gitlab.com';
+  if (argv.verbose) {
+    console.log(`Set gitlab url to ${baseUrl}`);
+  }
+  console.log();
+  if (!argv.token) {
+    console.log(
+      `Please pass your gitlab token using the --token flag,\nGet your token at ${baseUrl}/profile/personal_access_tokens\n\npass --help for full help\n\n`
+    );
+    process.exit(1);
+  }
+
+  const method = argv.method == 'ssh' ? 'ssh_url_to_repo' : 'http_url_to_repo';
+  const requestOptions = {
+    json: true,
+    qs: {
+      simple: true
+    },
+    headers: {
+      'PRIVATE-TOKEN': argv.token
+    }
+  };
+
+  const user = await rp.get(`${baseUrl}/api/v4/user`, requestOptions);
+  if (argv.verbose) {
+    console.log(`Got user: ${user.name} (${user.username}) ID: ${user.id}`);
+  }
+
+  issues = [];
+  page = 1;
+  while (!_.isEmpty(issuePage = await rp.get(
+        `${baseUrl}/api/v4/issues?scope=all&page=${page} `,
+        requestOptions
+    ))) {
+      console.log(`Reading page: ${page}`)
+      issues = _.concat(issues, issuePage);
+      page ++;
+  }
+  if (argv.verbose) {
+    console.log(
+      `Got all issues: count=${issues.length}\n`,
+      issues.map(i => `ProjectId: ${i.project_id}, Id ${i.id}`)
+    );
+  }
+
+  // save to file
+
+};
+
+module.exports = runGetIssues;
